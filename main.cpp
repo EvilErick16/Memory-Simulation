71b19894e13727520a8b8736a3dc5c5e9aa59c31
Erick Juarez <juarez.erick16@csu.fullerton.edu>
2019-12-01 22:11:00 -0800

    Added project files

---
+++ main.cpp
@@ -0,0 +1,30 @@
+// Erick Juarez 
+// Juarez.erick16@csu.fullerton.edu
+// CPSC 351 Sec 01 - Operating Systems
+// Assignment 2 - Memory Manager Simulation
+
+// Include libraries
+# include <iostream>
+
+
+////////////////////////////////////////////////////////////////////////////////////
+// This file contains the main function of the program
+/////////////////////////////////////////////////////////////////////////////////////
+
+/*    Pseudo code for main 
+    1. get memory size
+    2. find valid factors to memoy size to be the page size 
+    3. let USER pick page size from valid factors fuoind in step 2 
+    4. open INPUT File
+    5. get process information and initialize PROCESS objcts
+    6. Initialize MEMORY and MEMORY MANAGER objects
+
+*/
+
+int main(){
+
+    
+    
+
+    return 0; 
+}
